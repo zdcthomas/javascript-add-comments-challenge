e70c9adb552d187f85dc092c3bffe3a1fde203c2
Becki Srofe <blf_23@yahoo.com>
2017-11-28 20:12:50 -0700

    Add form, verified that variables are saving correct values

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-  // add your code here
+  $( '#new_comment_button' ).on('click', function() {
+    $('#new_comment_button').hide()
+    var formComment = '<textarea name="comment" id="comment" placeholder="Write your comment"></textarea></br>';
+    var formAuthor = '<input type="text" name="author" id="author" placeholder="Author" /></br>';
+    var formSubmit = '<input type="submit" value="Submit Comment" />';
+    $('#comments').append('<form id ="new_comment_form">' + formComment + formAuthor + formSubmit + '</form>');
+    $('form#new_comment_form').on('submit', function(){
+      var comment = $('textarea#comment').val();
+      var author = $('input#author').val();
+      alert(comment)
+    })
+  });
 })
