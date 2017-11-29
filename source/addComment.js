1bb71525f5f4418627bccd22c219ccf28c2ff3dc
Becki Srofe <blf_23@yahoo.com>
2017-11-28 20:33:53 -0700

    It works

--- source/addComment.js
+++ source/addComment.js
@@ -1,14 +1,19 @@
 $(document).ready(function(){
   $( '#new_comment_button' ).on('click', function() {
-    $('#new_comment_button').hide()
     var formComment = '<textarea name="comment" id="comment" placeholder="Write your comment"></textarea></br>';
     var formAuthor = '<input type="text" name="author" id="author" placeholder="Author" /></br>';
     var formSubmit = '<input type="submit" value="Submit Comment" />';
     $('#comments').append('<form id ="new_comment_form">' + formComment + formAuthor + formSubmit + '</form>');
-    $('form#new_comment_form').on('submit', function(){
+    $('#new_comment_form').on('submit', function(event){
       var comment = $('textarea#comment').val();
       var author = $('input#author').val();
-      alert(comment)
+      if (comment.length === 0) {
+        alert("You can't submit without a comment!")
+      } else {
+        $('ul').append('<li>' + comment + "<span class='author'>" + author + '</span></li>')
+        event.preventDefault()
+      }
+      $('#new_comment_form').remove()
     })
   });
 })
