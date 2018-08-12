9052e7a7a52670d4c2dd7e2977dc778d2fd2aff5
zdcthomas <zdcthomas@yahoo.com>
2018-08-12 17:07:12 -0500

    implements adding comments

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-  // add your code here
+  $('#new_comment_button').click( (event)=>{
+    $('#new_comment_button').css('display', 'none')
+    $('#new_comment').css('display', 'block')
+  })
+
+  $('#submit').click( ()=>{
+    $('#new_comment_button').css('display', 'block')
+    $('#new_comment').css('display', 'none')
+    let content = $('textarea').val()
+    let authorVal = $('#authorName').val()
+    let author = authorVal ? `<span class="author">${authorVal}</span>` : ""
+    $(`<li> ${content} ${author}</li>`).appendTo('#comment_list')
+    debugger
+  })
 })
